fb916306146bdd7e90087f84f52bb0a36f1c03d8
Sanjusuriya <sanjusuriya08@gmail.com>
2016-10-11 18:49:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,101 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles = {
+    'article-one': {
+        title: 'Article one | sooraj krish',
+        heading: 'Article one',
+        date: 'Sep 25, 2016',
+        content:`
+                <p>
+                    This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article..
+                </p>
+                <p>
+                    This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+                </p>
+                <p>
+                    This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+                </p>`
+    },
+    'article-two': {title: 'Article two | sooraj krish',
+        heading: 'Article two',
+        date: 'Sep 25, 2016',
+        content:`
+                <p>
+                    This is the content of my second article.
+                </p>
+                <p>
+                    This is the content of my second article.
+                </p>`},
+    'article-three': {
+        title: 'Article three | sooraj krish',
+        heading: 'Article three',
+        date: 'Sep 25, 2016',
+        content:`
+                <p>
+                    This is the content of my third article.
+                </p>
+                <p>
+                    This is the content of my third article.
+                </p>`
+                }
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <div>    
+                <a href="/">home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;
+app.get('/counter', function(req, res){
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+app.get('/:article', function (req, res) {
+    var articleName = req.params.article;
+   res.send(createTemplate(articles[articleName]));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/style.css', function (req, res) {
